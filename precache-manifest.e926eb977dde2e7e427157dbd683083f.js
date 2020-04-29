81bcf52103f5cfd38c3323d54ea3fe8bb24c79aa
malumart <malu.martin92@hotmail.com>
2020-04-29 19:06:09 -0300

    Updates

---
+++ precache-manifest.e926eb977dde2e7e427157dbd683083f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dfe20b10f9bb9ac12975162534fad23e",
+    "url": "/covid-19-react/index.html"
+  },
+  {
+    "revision": "361c00949c66962d6a31",
+    "url": "/covid-19-react/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "88c5b9d4a61560bc9cea",
+    "url": "/covid-19-react/static/js/2.06f0008a.chunk.js"
+  },
+  {
+    "revision": "4845593b3544c4878bbf7b7c87ce3e13",
+    "url": "/covid-19-react/static/js/2.06f0008a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "361c00949c66962d6a31",
+    "url": "/covid-19-react/static/js/main.dd608bcf.chunk.js"
+  },
+  {
+    "revision": "a13aa3692532e3ca7f88",
+    "url": "/covid-19-react/static/js/runtime-main.34a5474c.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/covid-19-react/static/media/covidimage.d7265326.png"
+  }
+]);
